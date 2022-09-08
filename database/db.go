f501ad5c529dbd5da71fa5067d3813c4c406bdef
Natanael <dev.natanael.dev@gmail.com>
2022-09-08 12:50:53 -0300

    atualizado

--- database/db.go
+++ database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/Natanael-devops/api-orcamento-familiar/models"
 	"gorm.io/driver/postgres"
@@ -14,11 +15,17 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
-	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+	if os.Getenv("ENV") == "PROD" {
+		ambiente := os.Getenv("DATABASE_URL")
+		DB, err = gorm.Open(postgres.Open(ambiente), &gorm.Config{})
+		if err != nil {
+			log.Panic("Erro ao conectar com banco de dados")
+		}
+	} else {
+		stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+		DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	}
+
 	DB.AutoMigrate(&models.Receita{})
 	DB.AutoMigrate(&models.Despesa{})
 	DB.AutoMigrate(&models.Usuario{})
